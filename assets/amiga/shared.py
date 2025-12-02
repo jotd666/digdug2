3f393d9cfec0ab5f9b9597de7f1a956da434d333
Jean-François Fabre <jotd666@gmail.com>
2025-12-02 23:03:48 +0100

    fixed hose

--- assets/amiga/shared.py
+++ assets/amiga/shared.py
@@ -93,7 +93,7 @@ def get_sprite_names():
 def get_double_size_y_sprites():
     return set(range(0x20,0x38,2))   # drilling hero
 def get_double_size_x_sprites():
-    game_grouped = {0X59,0x5D,0x64,0x5c,0x61,0x65,0x60}  # fire spitting dragon, flame
+    game_grouped = {0X59,0x5D,0x64,0x5c,0x61,0x65,0x60,0x75}  # fire spitting dragon, flame, hose
     extra_grouped = {0x3C,0xE0,0xE2,0xE6,0xE8,0xEA}  # will speed up display
     rval = {x:True for x in game_grouped}
     rval.update({x:False for x in extra_grouped})
