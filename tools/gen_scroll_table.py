bf0d143e5447886181ad5cc560b9231a3b14fd55
Jean-François Fabre <jotd666@gmail.com>
2025-12-12 22:42:11 +0100

    ECS version running with sprites distorted

--- tools/gen_scroll_table.py
+++ tools/gen_scroll_table.py
@@ -9,19 +9,21 @@ src_dir = this_dir / os.pardir / "src" / "amiga"
 
 
 # table with fine shift + byte offset
-def doit(width):
+def doit(width,dual_playfield):
     static_x_shift = width // 2  # shift other bitplane by half to get clipping effect on sprites
     asm_output = src_dir / f"scroll_table_{width}.68k"
     wmask = width-1
     scroll_table = [0]*512
 
-    static_lsb = (static_x_shift-1) >> 4
-    static_msb = (static_x_shift-1) & 0xF
+
+    if dual_playfield:
+        static_lsb = (static_x_shift-1) >> 4
+        static_msb = (static_x_shift-1) & 0xF
 
 
-    # bob playfield
-    other_plane_mask_1 = static_lsb << 2
-    other_plane_mask_2 = static_msb
+        # bob playfield
+        other_plane_mask_1 = static_lsb << 2
+        other_plane_mask_2 = static_msb
 
 
     items = []
@@ -31,8 +33,12 @@ def doit(width):
         # pre-encode shift for bplcon
 
         shiftval_msb = ((shift&(wmask & 0x30))>>2)      # 2 high bits H7 H6
-        items.append((shiftval_msb<<4)|other_plane_mask_1)
         shiftval_lsb = (shift&0xF)                      # 4 low bits H5->H2
+        if not dual_playfield:
+            other_plane_mask_1 = shiftval_lsb << 2
+            other_plane_mask_2 = shiftval_msb
+
+        items.append((shiftval_msb<<4)|other_plane_mask_1)
         items.append((shiftval_lsb<<4)|other_plane_mask_2)
 
         items.append(0)
@@ -44,5 +50,5 @@ def doit(width):
     return items
 
 if __name__ == "__main__":
-    doit(width = 64)   # FMODE=3
-    doit(width = 16)   # FMODE=0
+    doit(width = 64, dual_playfield = True)   # FMODE=3
+    doit(width = 16, dual_playfield = False)   # FMODE=0
